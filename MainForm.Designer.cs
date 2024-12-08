1c3f3b9ae42ecf4cf28f9ac962541acf5f71abc7
Shanley <sebialshanleyflores@gmail.com>
2024-12-08 23:17:59 +0800

    modified

---
+++ MainForm.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace back_propagation
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            tbInput1 = new TextBox();
+            tbInput2 = new TextBox();
+            tbInput3 = new TextBox();
+            tbInput4 = new TextBox();
+            btnCreate = new Button();
+            btnTrain = new Button();
+            btnTest = new Button();
+            tbOutput = new TextBox();
+            tbEpoch = new TextBox();
+            tbThreshold = new TextBox();
+            Threshold = new Label();
+            Epoch = new Label();
+            Output = new Label();
+            Input = new Label();
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+
+            // 
+            // tbInput1
+            // 
+            tbInput1.Location = new Point(52, 75);
+            tbInput1.Name = "tbInput1";
+            tbInput1.Size = new Size(142, 23);
+            tbInput1.TabIndex = 0;
+
+            // 
+            // tbInput2
+            // 
+            tbInput2.Location = new Point(52, 110);
+            tbInput2.Name = "tbInput2";
+            tbInput2.Size = new Size(142, 23);
+            tbInput2.TabIndex = 1;
+
+            // 
+            // tbInput3 (initially disabled for OR)
+            // 
+            tbInput3.Location = new Point(52, 145);
+            tbInput3.Name = "tbInput3";
+            tbInput3.Size = new Size(142, 23);
+            tbInput3.TabIndex = 2;
+            tbInput3.Enabled = false; // Disabled for OR
+
+            // 
+            // tbInput4 (initially disabled for OR)
+            // 
+            tbInput4.Location = new Point(52, 180);
+            tbInput4.Name = "tbInput4";
+            tbInput4.Size = new Size(142, 23);
+            tbInput4.TabIndex = 3;
+            tbInput4.Enabled = false; // Disabled for OR
+
+            // 
+            // btnCreate
+            // 
+            btnCreate.Location = new Point(53, 231);
+            btnCreate.Name = "btnCreate";
+            btnCreate.Size = new Size(142, 30);
+            btnCreate.TabIndex = 4;
+            btnCreate.Text = "Create";
+            btnCreate.UseVisualStyleBackColor = true;
+            btnCreate.Click += btnCreate_Click;
+
+            // 
+            // btnTrain
+            // 
+            btnTrain.Location = new Point(240, 231);
+            btnTrain.Name = "btnTrain";
+            btnTrain.Size = new Size(142, 30);
+            btnTrain.TabIndex = 5;
+            btnTrain.Text = "Train";
+            btnTrain.UseVisualStyleBackColor = true;
+            btnTrain.Click += btnTrain_Click;
+
+            // 
+            // btnTest
+            // 
+            btnTest.Location = new Point(426, 231);
+            btnTest.Name = "btnTest";
+            btnTest.Size = new Size(142, 30);
+            btnTest.TabIndex = 6;
+            btnTest.Text = "Test";
+            btnTest.UseVisualStyleBackColor = true;
+            btnTest.Click += btnTest_Click;
+
+            // 
+            // tbOutput
+            // 
+            tbOutput.Location = new Point(426, 180);
+            tbOutput.Name = "tbOutput";
+            tbOutput.Size = new Size(142, 23);
+            tbOutput.TabIndex = 7;
+
+            // 
+            // tbEpoch
+            // 
+            tbEpoch.Location = new Point(240, 180);
+            tbEpoch.Name = "tbEpoch";
+            tbEpoch.Size = new Size(142, 23);
+            tbEpoch.TabIndex = 8;
+
+            // 
+            // tbThreshold
+            // 
+            tbThreshold.Location = new Point(240, 130);
+            tbThreshold.Name = "tbThreshold";
+            tbThreshold.Size = new Size(142, 23);
+            tbThreshold.TabIndex = 9;
+
+            // 
+            // Threshold
+            // 
+            Threshold.AutoSize = true;
+            Threshold.Location = new Point(235, 108);
+            Threshold.Name = "Threshold";
+            Threshold.Size = new Size(59, 15);
+            Threshold.TabIndex = 10;
+            Threshold.Text = "Threshold";
+
+            // 
+            // Epoch
+            // 
+            Epoch.AutoSize = true;
+            Epoch.Location = new Point(237, 162);
+            Epoch.Name = "Epoch";
+            Epoch.Size = new Size(40, 15);
+            Epoch.TabIndex = 11;
+            Epoch.Text = "Epoch";
+
+            // 
+            // Output
+            // 
+            Output.AutoSize = true;
+            Output.Location = new Point(423, 161);
+            Output.Name = "Output";
+            Output.Size = new Size(45, 15);
+            Output.TabIndex = 12;
+            Output.Text = "Output";
+
+            // 
+            // Input
+            // 
+            Input.AutoSize = true;
+            Input.Location = new Point(49, 52);
+            Input.Name = "Input";
+            Input.Size = new Size(35, 15);
+            Input.TabIndex = 13;
+            Input.Text = "Input";
+
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(650, 12);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(142, 23);
+            comboBox1.TabIndex = 14;
+
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(comboBox1);
+            Controls.Add(Input);
+            Controls.Add(Output);
+            Controls.Add(Epoch);
+            Controls.Add(Threshold);
+            Controls.Add(tbThreshold);
+            Controls.Add(tbEpoch);
+            Controls.Add(tbOutput);
+            Controls.Add(btnTest);
+            Controls.Add(btnTrain);
+            Controls.Add(btnCreate);
+            Controls.Add(tbInput4);
+            Controls.Add(tbInput3);
+            Controls.Add(tbInput2);
+            Controls.Add(tbInput1);
+            Name = "MainForm";
+            Text = "Backpropagation Neural Network";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tbInput1;
+        private TextBox tbInput2;
+        private TextBox tbInput3;
+        private TextBox tbInput4;
+        private Button btnCreate;
+        private Button btnTrain;
+        private Button btnTest;
+        private TextBox tbOutput;
+        private TextBox tbEpoch;
+        private TextBox tbThreshold;
+        private Label Threshold;
+        private Label Epoch;
+        private Label Output;
+        private Label Input;
+        private ComboBox comboBox1;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Windows.Forms;
+using Backprop;
+
+namespace back_propagation
+{
+    public partial class MainForm : Form
+    {
+        NeuralNet nn;
+        int inputSize = 2;
+        int outputSize = 1;
+        int hiddenNeuron;
+        int min_epoch = 10000;
+        int curr_epoch;
+        int[,] orDataSet = new int[4, 3]
+        {
+            { 0, 0, 0 },
+            { 0, 1, 1 },
+            { 1, 0, 1 },
+            { 1, 1, 1 }
+        };
+
+        int[,] andDataSet = new int[16, 5]
+        {
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 1, 0 },
+            { 0, 0, 1, 0, 0 },
+            { 0, 0, 1, 1, 0 },
+            { 0, 1, 0, 0, 0 },
+            { 0, 1, 0, 1, 0 },
+            { 0, 1, 1, 0, 0 },
+            { 0, 1, 1, 1, 0 },
+            { 1, 0, 0, 0, 0 },
+            { 1, 0, 0, 1, 0 },
+            { 1, 0, 1, 0, 0 },
+            { 1, 0, 1, 1, 0 },
+            { 1, 1, 0, 0, 0 },
+            { 1, 1, 0, 1, 0 },
+            { 1, 1, 1, 0, 0 },
+            { 1, 1, 1, 1, 1 }
+        };
+
+        int[,] dataSet;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(new string[] { "OR", "AND" });
+            comboBox1.SelectedIndex = 1; 
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "OR")
+            {
+                dataSet = orDataSet;
+                inputSize = 2;
+                outputSize = 1;
+                tbInput3.Enabled = false;
+                tbInput4.Enabled = false;
+            }
+            else if (comboBox1.SelectedItem.ToString() == "AND")
+            {
+                dataSet = andDataSet;
+                inputSize = 4;
+                outputSize = 1;
+                tbInput3.Enabled = true;
+                tbInput4.Enabled = true;
+            }
+
+            CreateNN();
+        }
+
+        private void CreateNN()
+        {
+            hiddenNeuron = (int)((2 / 3.0) * inputSize) + outputSize;
+            nn = new NeuralNet(inputSize, hiddenNeuron, outputSize);
+        }
+
+        private void btnTrain_Click(object sender, EventArgs e)
+        {
+            if (nn == null)
+            {
+                CreateNN();
+            }
+
+            if (curr_epoch == 0)
+            {
+                for (int i = 1; i <= min_epoch; i++)
+                {
+                    trainNN();
+                    double mad = testTrainNN();
+                    if (mad <= 0.01)
+                    {
+                        min_epoch = i;
+                        break;
+                    }
+                }
+
+                curr_epoch = min_epoch;
+            }
+            else
+            {
+                int epoch = Convert.ToInt32(tbEpoch.Text);
+                for (int i = 0; i < epoch; i++)
+                {
+                    trainNN();
+                }
+
+                curr_epoch += epoch;
+            }
+
+            MessageBox.Show($"Training complete! Epochs: {curr_epoch}", "Training", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void trainNN()
+        {
+            for (int i = 0; i < Math.Pow(2, inputSize); i++)
+            {
+                for (int j = 0; j < inputSize; j++)
+                {
+                    nn.setInputs(j, dataSet[i, j]);
+                }
+
+                nn.setDesiredOutput(0, dataSet[i, inputSize]);
+                nn.learn();
+            }
+        }
+
+        private double testTrainNN()
+        {
+            double errorSum = 0;
+
+            for (int i = 0; i < Math.Pow(2, inputSize); i++)
+            {
+                for (int j = 0; j < inputSize; j++)
+                {
+                    nn.setInputs(j, dataSet[i, j]);
+                }
+
+                nn.run();
+                errorSum += Math.Abs((double)dataSet[i, inputSize] - nn.getOuputData(0));
+            }
+
+            return errorSum / Math.Pow(2, inputSize);
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "OR")
+            {
+                nn.setInputs(0, Convert.ToDouble(tbInput1.Text));
+                nn.setInputs(1, Convert.ToDouble(tbInput2.Text));
+            }
+            else if (comboBox1.SelectedItem.ToString() == "AND")
+            {
+                nn.setInputs(0, Convert.ToDouble(tbInput1.Text));
+                nn.setInputs(1, Convert.ToDouble(tbInput2.Text));
+
+                if (tbInput3.Enabled && tbInput4.Enabled)
+                {
+                    nn.setInputs(2, Convert.ToDouble(tbInput3.Text));
+                    nn.setInputs(3, Convert.ToDouble(tbInput4.Text));
+                }
+                else
+                {
+                    MessageBox.Show("Please enable tbInput3 and tbInput4 for AND dataset", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            nn.run();
+            tbOutput.Text = "" + nn.getOuputData(0);
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            CreateNN();
+            curr_epoch = 0;
+            MessageBox.Show("Neural Network Created!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            min_epoch = 10000;
+        }
+    }
+}
